b2ded8bdf44db7c503c934e619f6970d68205d21
TomasGutierrez777 <togutierrez2023@udec.cl>
2025-08-30 19:36:56 -0400

    GREEN: implementación mínima para el dado y su lanzamiento

--- src/game/dice.py
+++ src/game/dice.py
@@ -1,2 +1,16 @@
+from random import randint
+
 class Dice:
-    pass
+    def __init__(self):
+        self.value = None
+
+    def roll(self):
+        """Genera un valor, lo almacena y guarda el nombre respectivo del valor
+
+        Returns:
+            int: valor generado por el dado
+        """
+        generated_value = randint(1, 6)
+        self.value = generated_value
+
+        return generated_value
