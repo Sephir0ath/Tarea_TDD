27b80558f9659bd6a88a9e3df522396391ce360e
TomasGutierrez777 <togutierrez2023@udec.cl>
2025-08-30 22:37:55 -0400

    GREEN: Implementación mínima para contar las pintas (no tiene en cuenta ases)

--- src/game/contador_pintas.py
+++ src/game/contador_pintas.py
@@ -1,2 +1,11 @@
-class ContadorPintas():
-    pass
+class ContadorPintas:
+    def contar_pinta(self, dice_value, cachos):
+        counter = 0
+        for cacho in cachos:
+            cacho_dices = cacho.get_dices()
+            for dice in cacho_dices:
+                if dice.value == dice_value:
+                    counter += 1
+                print(dice.value)
+
+        return counter
