0c88dbcb4f5cc7ae26fdf382a5e1e0e2285c4eb0
Sephir0ath <feliperaysz3@gmail.com>
2025-08-31 15:17:30 -0400

    REFACTOR: Cambios en el código para manejar eliminación de jugador, también se arregló error en return de los índices para métodos handle_calzo y handle_doubt

--- src/game/round_referee.py
+++ src/game/round_referee.py
@@ -11,6 +11,9 @@ class RoundReferee:
         Maneja la lógica cuando un jugador duda, primero determina qué jugador perdió,
         luego si es que la ronda actual era una ronda especial, resetea a las rondas normales,
         después quita un dado al perdedor y verifica si no hay que activar ronda especial o si hay que eliminar a un jugador.
+
+        Returns:
+            int: valor del jugador que empezará la siguiente ronda
         """
         if self.counter.contar_pinta(self.current_bet[1], players, self.is_special_round) >= self.current_bet[0]:
             loser_index = current_player  # El que dudó pierde
@@ -25,6 +28,8 @@ class RoundReferee:
 
         if dice_count == 0:
             players.pop(loser_index)
+            next_player = loser_index % len(players)
+            return next_player
 
         elif dice_count == 1 and not special_round_list[loser_index]:
             special_round_list[loser_index] = True
@@ -32,13 +37,32 @@ class RoundReferee:
 
         return loser_index
 
+
     def handle_calzo(self, players, current_player, special_round_list):
+        """
+        Maneja la lógica cuando un jugador calza, actúa similar a handle_doubt
+        con la excepción de que sólo el jugador actual puede ganar o perder dados.
+
+        Returns:
+            int: valor del jugador que empezará la siguiente ronda
+        """
         if self.counter.contar_pinta(self.current_bet[1], players, self.is_special_round) == self.current_bet[0]:
             players[current_player].add_dice() # el que calzó gana un dado
         else:
             players[current_player].remove_dice()
 
+        if self.is_special_round:
+            self.is_special_round = False
 
+        dice_count = len(players[current_player].get_dices())
+        if dice_count == 0:
+            players.pop(current_player)
+            next_player = current_player % len(players)
+            return next_player
+
+        elif dice_count == 1 and not special_round_list[current_player]:
+            special_round_list[current_player] = True
+            self.is_special_round = True
 
         return current_player
 
