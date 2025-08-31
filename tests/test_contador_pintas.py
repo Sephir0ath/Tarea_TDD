5a4be41ba2382a6bb19be0dfac229829ce426a28
TomasGutierrez777 <togutierrez2023@udec.cl>
2025-08-30 22:23:12 -0400

    Red: Test para contar las pintas de la pinta que se especifique

---
+++ tests/test_contador_pintas.py
@@ -0,0 +1,31 @@
+import unittest
+
+from src.game.contador_pintas import ContadorPintas
+from src.game.cacho import Cacho
+from src.game.enums import DiceNames
+from unittest.mock import MagicMock
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+class TestContadorPintas(unittest.TestCase):
+    def setUp(self):
+        self.contador_pintas = ContadorPintas()
+        self.cachos_list = []
+
+        for i in range(4):  # Crear 4 jugadores (cachos)
+            self.cachos_list.append(Cacho())
+            self.cachos_list[i].roll_dices()
+
+    def test_contar_apariciones_pinta(self):
+        for i in self.cachos_list:
+            for x in range(5):
+                if x == 0:
+                    i.get_dices()[x].value = 2
+                else:
+                    i.get_dices()[x].value = 3
+
+
+        tontos_quantity = self.contador_pintas.contar_pinta(2, self.cachos_list)
+        self.assertEqual(tontos_quantity, 4)
