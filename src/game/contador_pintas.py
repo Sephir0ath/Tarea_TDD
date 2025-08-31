8286f756634c338305f7441428f87869e32f5dd1
TomasGutierrez777 <togutierrez2023@udec.cl>
2025-08-30 23:06:16 -0400

    GREEN: Implementación mínima para contar ases como comodines

--- src/game/contador_pintas.py
+++ src/game/contador_pintas.py
@@ -1,11 +1,12 @@
+
 class ContadorPintas:
-    def contar_pinta(self, dice_value, cachos):
+    def contar_pinta(self, dice_value, cachos, is_special_round=True):
         counter = 0
         for cacho in cachos:
             cacho_dices = cacho.get_dices()
             for dice in cacho_dices:
-                if dice.value == dice_value:
+                if dice.value == dice_value or (not is_special_round and dice.value == 1):
                     counter += 1
-                print(dice.value)
+
 
         return counter
