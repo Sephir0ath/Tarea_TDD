57882cf892308fec0a14e988c2fb281d1dac1f00
Sephir0ath <feliperaysz3@gmail.com>
2025-08-31 17:09:16 -0400

    GREEN: implementación mínima para setear una apuesta inicial

--- src/game/bet_validator.py
+++ src/game/bet_validator.py
@@ -1,2 +1,12 @@
 class BetValidator:
-    pass
+    def __init__(self):
+        self.current_bet = None
+
+    def check_bet(self, new_bet):
+        if self.current_bet is None:
+            self.current_bet = new_bet
+            return True
+        else:
+            return False
+
+        return False
