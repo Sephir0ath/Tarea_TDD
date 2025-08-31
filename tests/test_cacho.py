9b5e21443ae386b78d8fc71443b31491d1ff9866
TomasGutierrez777 <togutierrez2023@udec.cl>
2025-08-30 21:06:52 -0400

    RED: Test para verificar que los cachos lanzan correctamente valores entre 1 y 6

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -9,6 +9,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'
 class TestCacho(unittest.TestCase):
     def setUp(self):
         self.cacho = Cacho()
+        self.cacho.roll_dices()
 
     def test_cacho_has_five_dices(self):
         self.assertEqual(len(self.cacho.get_dices()), 5)
+
+    def test_cacho_can_roll_values(self):
+        self.cacho.roll_dices()
+
+        new_values = []
+        for dice in self.cacho.get_dices():
+            new_values.append(dice.value)
+
+        for dice_value in new_values:
+            assert 1 <= dice_value <= 6
