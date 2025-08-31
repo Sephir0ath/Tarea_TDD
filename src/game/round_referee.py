0be7ee74f85a0aa3322ea342babab6555f3c5acd
Matfigueroa20 <matfigueroa2023@udec.cl>
2025-08-31 01:01:21 -0400

    GREEN: Implementación mínima para manejar el jugador que pierde un dado al dudar

--- src/game/round_referee.py
+++ src/game/round_referee.py
@@ -1,2 +1,14 @@
+from .contador_pintas import ContadorPintas
+
 class RoundReferee:
-    pass
+    def __init__(self):
+        self.current_bet = ()
+        self.is_special_round = False
+        self.counter = ContadorPintas()
+
+    def handle_doubt(self, players, current_player):
+        if self.counter.contar_pinta(self.current_bet[1], players, self.is_special_round) >= self.current_bet[0]:
+            players[current_player].remove_dice()
+        else:
+            last_player = (current_player-1) % len(players)
+            players[last_player].remove_dice()
