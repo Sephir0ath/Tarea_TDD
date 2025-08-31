57b0952af3ec4015284300433e3b5ef57b31e9fb
Sephir0ath <feliperaysz3@gmail.com>
2025-08-31 15:41:19 -0400

    RED: Test para validar las condiciones necesarias para aceptar un calzo

--- tests/test_round_referee.py
+++ tests/test_round_referee.py
@@ -24,14 +24,14 @@ class TestRoundReferee(unittest.TestCase):
     def test_arbitro_maneja_dudo(self):
         # Pierde un dado el jugador anterior ya que solo hay trenes en los cachos
         self.referee.current_bet = (2, 2)
-        self.referee.handle_doubt(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round) # Pierde el jugador que hizo la apuesta (el anterior al actual)
+        self.referee.handle_doubt(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round, self.manager.player_quantity) # Pierde el jugador que hizo la apuesta (el anterior al actual)
 
-        last_player = self.manager.players[(self.manager.current_player - 1) % self.player_quantity]
+        last_player = self.manager.players[(self.manager.current_player - 1) % self.manager.player_quantity]
         self.assertEqual(len(last_player.get_dices()),  4) # Obtenemos la cantidad de dados que tiene actualmente el jugador anterior que hizo la apuesta
 
         # Pierde el jugador actual ya que hay al menos 4 trenes en total
         self.referee.current_bet = (4, 3)
-        self.referee.handle_doubt(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round)
+        self.referee.handle_doubt(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round, self.manager.player_quantity)
         current_player = self.manager.players[self.manager.current_player]
         self.assertEqual(len(current_player.get_dices()), 4)
 
@@ -39,17 +39,25 @@ class TestRoundReferee(unittest.TestCase):
         self.manager.players[2].dices = [Dice()]
         self.referee.current_bet = (4, 3)
 
-        self.referee.handle_doubt(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round)  # Pierde el jugador que hizo la apuesta (el anterior al actual)
+        self.referee.handle_doubt(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round, self.manager.player_quantity)  # Pierde el jugador que hizo la apuesta (el anterior al actual)
         self.assertEqual(len(self.manager.players), self.player_quantity-1)
 
     def test_arbitro_maneja_calzo(self):
         self.referee.current_bet = (self.player_quantity*5, 3) # La cantidad exacta de trenes que están en juego
-        self.referee.handle_calzo(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round)
+        self.referee.handle_calzo(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round, self.manager.player_quantity)
         actual_player_dice_quantity = len(self.manager.players[self.manager.current_player].dices)
         self.assertEqual(actual_player_dice_quantity, 5) # El jugador no pierde dados
 
 
-        self.referee.current_bet = (self.player_quantity*5, 2)
-        self.referee.handle_calzo(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round)
+        self.referee.current_bet = (self.manager.player_quantity*5, 2)
+        self.referee.handle_calzo(self.manager.players, self.manager.current_player, self.manager.has_activated_special_round, self.manager.player_quantity)
         actual_player_dice_quantity = len(self.manager.players[self.manager.current_player].dices)
         self.assertEqual(actual_player_dice_quantity, 4)  # El jugador pierde dado
+
+    def test_validar_posibilidad_de_calzo(self):
+        # El jugador actual no tiene un solo dado y tampoco están en juego menos de la mitad de los dados (condiciones de calzo)
+        self.assertFalse(self.referee.validate_calzo(self.manager.players, self.manager.current_player, self.manager.total_player_quantity))
+
+        self.referee.current_bet = (self.player_quantity*5, 3)
+        self.manager.players[self.manager.current_player].dices = [Dice()]
+        self.assertTrue(self.referee.validate_calzo(self.manager.players, self.manager.current_player, self.manager.total_player_quantity))
