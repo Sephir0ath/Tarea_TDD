7096d7c7d315f1e7d6ce2d78a4dbbab5d916950e
Sephir0ath <feliperaysz3@gmail.com>
2025-09-02 21:52:18 -0400

    REFACTOR: refactorización de los tests de dados para añadir mocking, funcionan de la misma manera

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock
 
 from src.game.enums import DiceNames
 from src.game.dice import Dice
@@ -10,7 +11,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'
 class TestDice(unittest.TestCase):
     def test_create_dice_with_value(self):
         dice = Dice()
-        dice.roll()
+        dice.roll = MagicMock(return_value=5)
+        result = dice.roll()
+        dice.value = result
         assert 1 <= dice.value <= 6
 
     def test_guardar_nombre_del_dado(self):
@@ -19,6 +22,9 @@ class TestDice(unittest.TestCase):
             valid_names.append(x.value)
 
         dice = Dice()
-        dice.roll()
+        dice.roll = MagicMock(return_value="As")
+        result = dice.roll()
+        dice.value = result
+        dice.value_name = DiceNames(result).value
 
         assert dice.value_name in valid_names
